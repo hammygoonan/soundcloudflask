23c1f11ac16b41dca574817585f75ea71cc4ea98
Mike Boehm <mike@boehm.com.au>
2014-04-05 15:29:58 +0100

    Player controls

--- static/scripts.js
+++ static/scripts.js
@@ -25,6 +25,20 @@ $(document).ready(function(){
 		embed_url = $(this).attr('data-embed')
 		$.get('/vlc?track=' + embed_url);
 	});
+	
+	$('.control-play').click(function(){
+		if(embed_url = $(this).attr('data-embed')) {
+				
+		} else {
+			embed_url = '';
+		}
+		$.get('/vlc?track=' + embed_url);
+	});
+
+	$('.control-stop').click(function(){
+		$.get('/stop_vlc');
+	});
+
 
 	var users = new Array()
 	$('.user').each(function(){
